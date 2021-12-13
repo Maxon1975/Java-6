cfb73d301e6275382bbbad43f21c177ff090abb2
Maxim Annenkov <bakeeva@yandex.ru>
2021-12-13 18:10:20 +0300

    345

---
+++ src/test/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,20 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int calculate(int min, int max) {
+
+        //int min = 200;
+        //int max = 300;
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= min && i * i <= max) {
+                count = count + 1;
+            }
+
+        }
+        return count;
+
+
+    }
+
+}
